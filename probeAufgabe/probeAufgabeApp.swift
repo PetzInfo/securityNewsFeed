178c514cbb73629b55524584c61953e00d4994ec
Friedrich Commichau <f.commichau@hm.edu>
2024-06-08 17:56:37 +0200

    Initial Commit

---
+++ probeAufgabe/probeAufgabeApp.swift
@@ -0,0 +1,20 @@
+//
+//  probeAufgabeApp.swift
+//  probeAufgabe
+//
+//  Created by Friedrich Commichau on 08.06.24.
+//
+
+import SwiftUI
+
+@main
+struct probeAufgabeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

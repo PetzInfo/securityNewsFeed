f0d4db8f600f26dce19216d204099d1563e786d7
Friedrich Commichau <f.commichau@hm.edu>
2024-06-10 10:32:12 +0200

    add news service to handle API data

--- probeAufgabe/services/NewsService.swift
+++ probeAufgabe/services/NewsService.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+class NewsService {
+    static let shared = NewsService()
+    private let url = URL(string: "https://staging-probeapi.mobile-software.ag/rohdaten/SecurtiyInfo.json")!
+    
+    func fetchNews(completion: @escaping (Result<[NewsItem], Error>) -> Void) {
+        var request = URLRequest(url: url)
+        let username = "aspirant"
+        let password = "mein-aller-bestes-probearbeiten!"
+        let loginString = String(format: "%@:%@", username, password)
+        let loginData = loginString.data(using: .utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+        
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "No data"])
+                completion(.failure(error))
+                return
+            }
+            
+            do {
+                let newsResponse = try JSONDecoder().decode(NewsResponse.self, from: data)
+                completion(.success(newsResponse.news))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}

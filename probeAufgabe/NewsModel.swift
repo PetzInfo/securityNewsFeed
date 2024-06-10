9bc19a4516f1517771b44ff388a773d4486f8ec4
Friedrich Commichau <f.commichau@hm.edu>
2024-06-10 10:20:08 +0200

    add message model for API

---
+++ probeAufgabe/NewsModel.swift
@@ -0,0 +1,34 @@
+//
+//  NewsModel.swift
+//  probeAufgabe
+//
+//  Created by Friedrich Commichau on 08.06.24.
+//
+
+import Foundation
+
+struct NewsResponse: Decodable {
+    let news: [NewsItem]
+}
+
+struct NewsItem: Decodable, Identifiable {
+    var id: UUID { UUID() } // unique ID for SwiftUI List
+    let articleID: Int
+    let category: Category
+    let imageURLs: [ImageURL]
+    let releasedate: String
+    let subtitle: String
+    let title: String
+    let video: Video?
+    
+    struct Category: Decodable {
+        let iconURL: String
+        let name: String
+    }
+    
+    struct ImageURL: Decodable {
+        let imageURL: String
+    }
+    
+    struct Video: Decodable {}
+}

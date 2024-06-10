39d3cd3e04e1f9e46963f0615c252881e571c752
Friedrich Commichau <f.commichau@hm.edu>
2024-06-10 11:54:34 +0200

    add read status as bool

--- probeAufgabe/models/NewsModel.swift
+++ probeAufgabe/models/NewsModel.swift
@@ -15,11 +15,12 @@ struct NewsItem: Codable, Identifiable {
     var id: UUID { UUID() } // unique ID for SwiftUI List
     let articleID: Int
     let category: Category
-    let imageURLs: [ImageURL]?
+    let imageURLs: [ImageURL]
     let releasedate: String
     let subtitle: String?
     let title: String
     let video: Video?
+    var isRead: Bool = false
     
     struct Category: Codable {
         let iconURL: String

d5f83e66aa39f792179166916149bb504565fb15
Friedrich Commichau <f.commichau@hm.edu>
2024-07-14 19:08:08 +0200

    implemeted usage of local Config.plist to store senstive data

--- probeAufgabe/services/FetchDataService.swift
+++ probeAufgabe/services/FetchDataService.swift
@@ -9,12 +9,29 @@ import Foundation
 
 class FetchDataService {
     static let shared = FetchDataService()
-    private let url = URL(string: "https://staging-probeapi.mobile-software.ag/rohdaten/SecurtiyInfo.json")!
+    
+    private let apiUrl: URL
+    private let username: String
+    private let password: String
+    
+    private init() {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let xml = FileManager.default.contents(atPath: path),
+              let config = try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil) as? [String: Any],
+              let apiUrlString = config["API_URL"] as? String,
+              let url = URL(string: apiUrlString),
+              let username = config["API_USERNAME"] as? String,
+              let password = config["API_PASSWORD"] as? String else {
+            fatalError("Failed to load configuration")
+        }
+        
+        self.apiUrl = url
+        self.username = username
+        self.password = password
+    }
     
     func fetchNews(completion: @escaping (Result<[NewsItem], Error>) -> Void) {
-        var request = URLRequest(url: url)
-        let username = "aspirant"
-        let password = "mein-aller-bestes-probearbeiten!"
+        var request = URLRequest(url: apiUrl)
         let loginString = String(format: "%@:%@", username, password)
         let loginData = loginString.data(using: .utf8)!
         let base64LoginString = loginData.base64EncodedString()
@@ -28,7 +45,7 @@ class FetchDataService {
             }
             
             guard let data = data else {
-                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "No data"])
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data"])
                 completion(.failure(error))
                 return
             }
